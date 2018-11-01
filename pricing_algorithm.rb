b07e59d04b132f436579c2453a1ca6cd95acc1dc
EslamMoh <eslammoh.ce@gmail.com>
2018-11-01 02:21:53 +0200

    Refactor pricing algorithm

--- pricing_algorithm.rb
+++ pricing_algorithm.rb
@@ -1,85 +1,34 @@
 if brand.rating = high
-  if price_reference < 15
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.75) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 25
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.775) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 50
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.8) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 75
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.825) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 100
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.85) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 200
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.875) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 400
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.9) * 1.1
-    price_original = price_suggestion
-  elsif price_reference < 600
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.925) * 1.1
-    price_original = price_suggestion
-  else
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.95) * 1.1
-    price_original = price_suggestion
+  validate_price_reference(1.1)
 elsif brand.rating = low
-  if price_reference < 15
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.75) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 25
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.775) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 50
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.8) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 75
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.825) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 100
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.85) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 200
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.875) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 400
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.9) * 0.9
-    price_original = price_suggestion
-  elsif price_reference < 600
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.925) * 0.9
-    price_original = price_suggestion
-  else
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.95) * 0.9
-    price_original = price_suggestion
+  validate_price_reference(0.9)
 else
+  validate_price_reference(1)
+end
+
+def suggest_price(n,y)
+  price_suggestion = (price_reference - product_database.shipping_cost * n) * y
+  price_original = price_suggestion
+end
+
+def validate_price_reference(m)
   if price_reference < 15
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.75) * 1
-    price_original = price_suggestion
+    suggest_price(0.75,m)
   elsif price_reference < 25
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.775) * 1
-    price_original = price_suggestion
+    suggest_price(0.775,m)
   elsif price_reference < 50
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.8) * 1
-    price_original = price_suggestion
+    suggest_price(0.8,m)
   elsif price_reference < 75
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.825) * 1
-    price_original = price_suggestion
+    suggest_price(0.825,m)
   elsif price_reference < 100
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.85) * 1
-    price_original = price_suggestion
+    suggest_price(0.85,m)
   elsif price_reference < 200
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.875) * 1
-    price_original = price_suggestion
+    suggest_price(0.875,m)
   elsif price_reference < 400
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.9) * 1
-    price_original = price_suggestion
+    suggest_price(0.9,m)
   elsif price_reference < 600
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.925) * 1
-    price_original = price_suggestion
+    suggest_price(0.925,m)
   else
-    price_suggestion = (price_reference - product_database.shipping_cost * 0.95) * 1
-    price_original = price_suggestion
+    suggest_price(0.95,m)
+  end 
 end
